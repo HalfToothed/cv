af00e7f8eb324f8ec80eb194c97eb46c7ecb5bef
HalfToothed <abhishekandmails@gmail.com>
2024-11-12 02:00:01 +0530

    updated nextConfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/cv",
-    output: "export",  // <=== enables static exports
-    distDir: 'docs',
+    basePath: "/cv", // Set base path for GitHub Pages
+    assetPrefix: "/cv", // Ensures all assets use the correct prefix
+    output: "export",  // Enables static exports
+    distDir: 'docs', // Outputs to `docs` for GitHub Pages
     reactStrictMode: true,
+    images: {
+        unoptimized: true, // Disable image optimization if needed
+    },
 }
 
 module.exports = nextConfig
